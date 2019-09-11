70f97494ef5c39a21e29e78f10e7e22f8d0ff39d
Laci <laszlo.roland.kiss@gmail.com>
2019-09-11 14:39:53 +0200

    add variables

---
+++ week-01/day-3/VARIABLES/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main (int argc, char* args []) {
+    // Favorite Number
+    //int favoriteNumber = 16;
+    //std::cout<< "My favorite number is: " << favoriteNumber << std::endl;
+
+
+    //Swap
+    //int a = 123;
+    //int b = 526;
+
+    //a = 526;
+    //b = 123;
+
+    //std::cout<< "a is now: " << a << std::endl;
+    //std::cout<< "b is now: " << b << std::endl;
+
+
+    //Bmi
+    //double massInKg = 81.2;
+    //double heightInM = 1.78;
+    //double bmi = massInKg / (heightInM * heightInM);
+
+    //std::cout<< bmi << std::endl;
+
+
+    //DefineBasicInfo
+    
+
+
+
+    return 0;
+
+}
