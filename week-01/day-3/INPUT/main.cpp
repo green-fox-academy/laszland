76a612c8c8d55f3b8a48196d3cd42a8e8d3e0c29
Laci <laszlo.roland.kiss@gmail.com>
2019-09-11 16:13:38 +0200

    add new task

---
+++ week-01/day-3/INPUT/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+    //Hello User
+    /*std::string userName;
+    std::cout<< "Please enter your name: ";
+    std::cin>> userName;
+
+    std::cout<< "Hello " << userName << "!"<< std::endl;*/
+
+
+    //Mile to Km Converter
+    /*double distMile;
+    std::cout<< "Please give me the distance in miles: ";
+    std::cin>> distMile;
+
+    double distKm = distMile * 1.6093;
+
+    std::cout<< distMile << " Miles is "<< distKm << " Km." << std::endl;*/
+
+
+    //Animals And Legs
+    /*int numChicken;
+    int numPig;
+
+    std::cout<< "Enter the number of chicken you own: ";
+    std::cin>> numChicken;
+
+    std::cout<< "Enter the number of pigs you own: ";
+    std::cin>> numPig;
+
+    int sumLeg = numChicken * 2 + numPig * 4;
+    std::cout<< "There are " << sumLeg << " legs in yor farm." << std::endl;*/
+
+
+    //Average Of Input
+    /*int num1;
+    int num2;
+    int num3;
+    int num4;
+    int num5;
+
+    std::cout<< "Type your first number: ";
+    std::cin>> num1;
+    std::cout<< "Type your second number: ";
+    std::cin>> num2;
+    std::cout<< "Type your third number: ";
+    std::cin>> num3;
+    std::cout<< "Type your forth number: ";
+    std::cin>> num4;
+    std::cout<< "Type your fifth number: ";
+    std::cin>> num5;
+
+    int sum = num1 + num2 + num3 + num4 + num5;
+    double ave = sum / 5;
+
+    std::cout<< "Sum: "<< sum << std::endl;
+    std::cout<< "Average: "<< ave << std:: endl;*/
+
+
+    
+
+
+    return 0;
+
+
+
+}
