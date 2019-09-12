da4a01f5aeea492640e76d47d957c720e6021331
Laci <laszlo.roland.kiss@gmail.com>
2019-09-12 22:00:06 +0200

    add new task

---
+++ week-01/day-5/ARRAYS/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main (int argc, char* args []) {
+
+    /*int myNumbers[] = {1, 2, 3, 6};
+
+    std::cout<< "The second element is: " << myNumbers[1] << std::endl;
+
+    for (int i = 0; i < sizeof(myNumbers) / sizeof(myNumbers[0]); i++) {
+        std::cout << myNumbers[i] << std::endl;
+    }*/
+
+    // THIRD
+    int q[] = {4, 5, 6, 7};
+
+    std::cout<< "The third element of q: " << q[3] << std::endl;
+
+    return 0;
+}
