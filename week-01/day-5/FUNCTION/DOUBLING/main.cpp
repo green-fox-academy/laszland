6613367fd96de98f0439a19128f64e3dc30679e5
Laci <laszlo.roland.kiss@gmail.com>
2019-09-13 14:45:06 +0200

    new task

---
+++ week-01/day-5/FUNCTION/DOUBLING/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int doubling(int x);
+
+int main (int argc, char* args[]) {
+
+    //DOUBLING
+    int baseNum = 123;
+    doubling(baseNum);*/
+
+    return 0;
+}
+
+int doubling(int x) {
+    x *= 2;
+    std::cout << x << std::endl;
+}
