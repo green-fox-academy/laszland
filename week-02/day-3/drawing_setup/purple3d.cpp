d9311e51144493864057489ce3b79a109c8fb15f
Laci <laszlo.roland.kiss@gmail.com>
2019-09-18 22:45:51 +0200

    finish task 3d

--- week-02/day-3/drawing_setup/purple3d.cpp
+++ week-02/day-3/drawing_setup/purple3d.cpp
@@ -1,13 +1,16 @@
 #include "draw.h"
 #include <iostream>
 
+int coord[6] = {0, 10, 30, 60, 100, 150};
+
 void draw(SDL_Renderer* gRenderer) {
-    for (int i = 1; i <= 12; i *= 2) {
+
+    for (int i = 0; i < 6; i++) {
         SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
-        SDL_Rect myRect1 = { i+10, i+10, i+10, i+10};
+        SDL_Rect myRect1 = { coord[i]-1, coord[i]-1, 10 + (10 * i), 10 + (10 * i)};
         SDL_RenderDrawRect (gRenderer, &myRect1);
         SDL_SetRenderDrawColor(gRenderer, 153, 43, 180, 255);
-        SDL_Rect myRect2 = { i+10+1, i+10+1, i+10-2, i+10-2};
+        SDL_Rect myRect2 = { coord[i], coord[i], 8 + (10 * i), 8 + (10 * i)};
         SDL_RenderFillRect (gRenderer, &myRect2);
     }
 
