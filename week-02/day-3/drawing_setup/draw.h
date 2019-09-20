348012f02df2a9f535701bcc2efbd2efaa8d51f8
Laci <laszlo.roland.kiss@gmail.com>
2019-09-20 11:12:11 +0200

    add triangle task

--- week-02/day-3/drawing_setup/draw.h
+++ week-02/day-3/drawing_setup/draw.h
@@ -6,10 +6,10 @@
 //Screen dimension constants
 //const int SCREEN_WIDTH = 640;
 //const int SCREEN_HEIGHT = 480;
-//const int SCREEN_WIDTH = 400;
-//const int SCREEN_HEIGHT = 400;
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 800;
+const int SCREEN_WIDTH = 400;
+const int SCREEN_HEIGHT = 400;
+//const int SCREEN_WIDTH = 800;
+//const int SCREEN_HEIGHT = 800;
 
 
 void draw(SDL_Renderer* gRenderer);
