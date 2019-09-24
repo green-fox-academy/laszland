9fb83ece7761cbcc00d2dc16dda711492a6151a6
Laci <laszlo.roland.kiss@gmail.com>
2019-09-24 16:27:21 +0200

    finish write multiple lines

--- week-03/day-2/Writing-files/main.cpp
+++ week-03/day-2/Writing-files/main.cpp
@@ -3,19 +3,33 @@
 #include <fstream>
 #include <string>
 
-int main() {
-    // Open a file called "my-file.txt"
-    // Write your name in it as a single line
+void writeMyFile (std::string file, std::string word, int numbLines);
 
+int main() {
+    // Create a function that takes 3 parameters: a path, a word and a number
+    // and is able to write into a file.
+    // The path parameter should be a string that describes the location of the file you wish to modify
+    // The word parameter should also be a string that will be written to the file as individual lines
+    // The number parameter should describe how many lines the file should have.
+    // If the word is "apple" and the number is 5, it should write 5 lines
+    // into the file and each line should read "apple"
+    std::string pathOfFile = "/Users/peter/greenfox/laszland/week-03/outputFile.txt";
+    std::string str = "banana";
+    int numberOfLines = 5;
+    writeMyFile(pathOfFile, str, numberOfLines);
+    return 0;
+}
+
+void writeMyFile (std::string file, std::string word, int numbLines) {
     std::ofstream myFile;
     myFile.exceptions(std::ofstream::failbit | std::ofstream::badbit);
     try {
-        myFile.open("my-file.txt");
-        std::string str = "My name is Laci.";
-        myFile << str;
+        myFile.open(file);
+        for (int i = 0; i < numbLines; i++) {
+            myFile << word << std::endl;
+        }
         myFile.close();
     } catch (std::ofstream::failure& e) {
         std::cout << e.what() << std::endl;
     }
-    return 0;
 }
