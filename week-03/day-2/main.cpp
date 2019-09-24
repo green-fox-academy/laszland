2c51ee2185a360bed8ccd690ad0ee999c6449052
Laci <laszlo.roland.kiss@gmail.com>
2019-09-24 13:43:13 +0200

    finish divide by zero

--- week-03/day-2/main.cpp
+++ week-03/day-2/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <fstream>
+
+void divideTen(int number);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Create a function that takes a number
+    // divides ten with it,
+    // and prints the result.
+    // It should print "fail" if the parameter is 0.
+    // Solve the excercise without using exceptions!
+
+    int numb = 2;
+    divideTen(numb);
+
     return 0;
+}
+
+void divideTen(int number)
+{
+    if (number == 0) {
+        std::cout << "fail" << std::endl;
+    } else {
+        std::cout << 10 / number << std::endl;
+    }
+
 }
