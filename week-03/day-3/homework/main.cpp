f96c1e5b7c3c45245ee8ae65b2f55007d553d5bc
Laci <laszlo.roland.kiss@gmail.com>
2019-09-24 21:06:46 +0200

    unfinished homework for day-3

---
+++ week-03/day-3/homework/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+std::vector<std::string> dataBase;
+
+int main() {
+    std::ifstream trees;
+    trees.open("trees.txt");
+    std::string dataOfTrees;
+    if (trees.is_open() == 1) {
+        while (getline(trees, dataOfTrees)) {
+            //std::cout << dataOfTrees << std::endl;
+            dataBase.push_back(dataOfTrees);
+        }
+    } else {
+        std::cout << "file couldn't be opened" << std::endl;
+    }
+    for (int i = 0; i < 4; i++) {
+        
+    }
+
+}
