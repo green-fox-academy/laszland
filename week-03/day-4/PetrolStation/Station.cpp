e4e217cd7d8905c0f670791ad1a9a6d724889f5f
Laci <laszlo.roland.kiss@gmail.com>
2019-09-26 17:06:31 +0200

    finish patrol station

---
+++ week-03/day-4/PetrolStation/Station.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Peter on 2019. 09. 26..
+//
+
+#include <iostream>
+#include "Station.h"
+
+class car {};
+
+Station::Station(int gasAmount) : _gasAmount(gasAmount) {}
+
+void Station::add(Car* car) {
+    cars.push_back(*car);
+}
+
+void Station::fill() {
+    for (int i = 0; i < cars.size(); i++ ) {
+        while (!cars[i].isFull()) {
+            cars[i].fill();
+            _gasAmount--;
+            std::cout << "Filling car!" << std::endl;
+
+        }
+        std::cout << _gasAmount << " unit gas left on the station." <<std::endl;
+    }
+}
