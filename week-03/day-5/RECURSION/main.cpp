a106ddd8d52b55418a2dbd34ab326cafce69575a
Laci <laszlo.roland.kiss@gmail.com>
2019-09-27 13:19:19 +0200

    finish power

--- week-03/day-5/RECURSION/main.cpp
+++ week-03/day-5/RECURSION/main.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 
-int addDigits(int n);
+int putPower(int n, int power);
 
 int main() {
 
     int numb;
+    int power;
 
-    std::cout << "Give me a non-negative integer: ";
+    std::cout << "Give me a positive integer: ";
     std::cin >> numb;
+    std::cout << std:: endl;
+    std::cout << "Give me the power integer: ";
+    std::cin >> power;
 
-    std::cout << "The sum of the yours numbers digits is: " << addDigits(numb);
-
-
+    std::cout << "Your number on the given power is: " << putPower(numb, power) << std::endl;
     return 0;
 }
 
-int addDigits(int n) {
-    if (n < 10) {
-        return n;
-    }
-    return n % 10 + (addDigits(n / 10));
+int putPower(int n, int power) {
+   if (power == 0) {
+       return 1;
+   }
+   return n * putPower(n, power - 1);
 }
