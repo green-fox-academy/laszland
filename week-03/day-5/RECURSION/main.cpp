9b34186b4c78fbd7192b7078c123c84f44484439
Laci <laszlo.roland.kiss@gmail.com>
2019-09-27 14:16:14 +0200

    finish greatest common divisor

--- week-03/day-5/RECURSION/main.cpp
+++ week-03/day-5/RECURSION/main.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 
-int putPower(int n, int power);
+int greatestCommonDivisor (int a, int b);
 
 int main() {
 
-    int numb;
-    int power;
+    // iterative
+    int a;
+    int b;
 
-    std::cout << "Give me a positive integer: ";
-    std::cin >> numb;
-    std::cout << std:: endl;
-    std::cout << "Give me the power integer: ";
-    std::cin >> power;
+    std::cout << "Give me a positive number: ";
+    std::cin >> a;
+    std::cout << "Give me another positive number: ";
+    std::cin >> b;
 
-    std::cout << "Your number on the given power is: " << putPower(numb, power) << std::endl;
-    return 0;
+    std::cout << "The greates common divisort of your numbers is: "
+              << greatestCommonDivisor(a, b) << std::endl;
+
+
+
 }
+/*
+int greatestCommonDivisor (int a, int b) {
+    int greatestDivisor;
+    if (a < b) {
+        if (a % (a - 1) == 0 || b % (a - 1) == 0) {
+            greatestDivisor = a - 1;
+        } greatestCommonDivisor(a - 1, b);
+    } else {
+        if (a % (b - 1) == 0 || b % (b - 1) == 0) {
+            greatestDivisor = b - 1;
+        } greatestCommonDivisor(a, b - 1);
+    }
+    return greatestDivisor;
+}
+ */
 
-int putPower(int n, int power) {
-   if (power == 0) {
-       return 1;
-   }
-   return n * putPower(n, power - 1);
+int greatestCommonDivisor (int a, int b)
+{
+    if (b == 0) {
+        return a;
+    } else {
+        return greatestCommonDivisor(b, a % b);
+    }
 }
