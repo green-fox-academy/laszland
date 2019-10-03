595602144b6673bb35860d20f32a3aad4127b869
Laci <laszlo.roland.kiss@gmail.com>
2019-10-03 15:11:25 +0200

    ToDoApp: finish Remove function

--- week-04/day-4/ToDoApp/main.cpp
+++ week-04/day-4/ToDoApp/main.cpp
@@ -51,7 +51,7 @@ void listTasks() {
 void addNewTask(std::string newTask) {
     // opening the listOfTask.txt
     std::ofstream myFile;
-    myFile.open("listOfTasks.txt");
+    myFile.open("listOfTasks.txt", std::ios::app);
     if (myFile.is_open()) {
         std::cout << "Opening file is succeeded!" <<std::endl;
         std::cout << std::endl;
@@ -65,18 +65,62 @@ void addNewTask(std::string newTask) {
     myFile.close();
 }
 
+void removeTask(int indexOfTask) {
+    // open source file
+    std::ifstream myFile;
+    myFile.open("listOfTasks.txt");
+    std::ofstream temp;
+    temp.open("temp.txt");
+    if(temp.is_open()) {
+        std::cout << "The temp.txt is open." << std::endl;
+    }
+    else {
+        std::cout << "There is an error with temp.txt." << std::endl;
+        }
 
-int main(int argc, char* args[]) {
+    int count = 1;
+    std::string listOfTasks;
+
+    while (getline(myFile, listOfTasks)) {
+        if (count != indexOfTask) {
+            temp << listOfTasks << std::endl;
+            //std::cout << "I copied the task item." << std::endl;
+            count++;
+        } else {
+            count++;
+            //std::cout << "I didn't copy the task item." << std::endl;
+        }
+    }
+    temp.close();
+    //std::cout << "I closed the temp.txt." << std::endl;
+    myFile.close();
+    //std::cout << "I closed the myFile." << std::endl;
+    remove("listOfTasks.txt");
+    //std::cout << "I removed the lostOfTask.txt." << std::endl;
+    rename("temp.txt", "listOfTasks.txt");
+    //std::cout << "I renamed temp.txt." << std::endl;
+}
+
+
+int main(int argc, char* argv[]) {
 
     if (argc == 1) {
         printUsage();
-    } else if (args[1] == std::string ("-l")) {
+    } else if (argv[1] == std::string ("-l")) {
         listTasks();
-    } else if (args[1] == std::string ("-a")) {
+    } else if (argv[1] == std::string ("-a")) {
         if (argc ==  2) {
-            std::cout << "You didn't added any task. Please, type your task in after -a!" << std::endl;
+            std::cout << "You didn't added any task. Please, type your task after -a!" << std::endl;
+        } else {
+            addNewTask(argv[2]);
         }
-        addNewTask(args[2]);
+    } else if (argv[1] == std::string ("-r")) {
+        if (argc == 2) {
+            std::cout << "Which task want you to remove? Please, type its index after -r!" << std::endl;
+        } else {
+            std::cout << std::stoi(argv[2]) << std::endl;
+            removeTask(std::stoi(argv[2]));
+        }
     }
 
     return 0;
