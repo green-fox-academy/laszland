19e25cfbcee989e9c180f4ad48425e6ea66b73bb
Laci <laszlo.roland.kiss@gmail.com>
2019-10-07 14:45:41 +0200

    finish Product Database

--- week-04/practiceAtHome/DataStructures/ProductDatabase.cpp
+++ week-04/practiceAtHome/DataStructures/ProductDatabase.cpp
@@ -4,6 +4,9 @@
 void howMuchIsTheFish(std::map<std::string, int> myMap);
 void mostExpensiveProduct(std::map<std::string, int> myMap);
 float averagePrise(std::map<std::string, int> myMap);
+void belowThreehundred(std::map<std::string, int> myMap);
+bool for125(std::map<std::string, int> myMap);
+void cheapestProduct(std::map<std::string, int> myMap);
 
 int main () {
 
@@ -11,7 +14,7 @@ int main () {
     products["eggs"] = 200;
     products["milk"] = 200;
     products["fish"] = 400;
-    products["apples"] = 150;
+    products["apples"] = 125;
     products["bread"] = 50;
     products["chicken"] = 550;
 
@@ -25,6 +28,15 @@ int main () {
     howMuchIsTheFish(products);
     mostExpensiveProduct(products);
     std::cout << "The average prise is: " << averagePrise(products) << std::endl;
+    belowThreehundred(products);
+
+    if (for125(products)) {
+        std::cout << "There is something for 125." << std::endl;
+    } else {
+        std::cout << "There is nothing for 125." << std::endl;
+    }
+
+    cheapestProduct(products);
 
     return 0;
 }
@@ -51,6 +63,41 @@ float averagePrise(std::map<std::string, int> myMap) {
         temp += it -> second;
     }
     return ((float)temp / myMap.size());
+}
+
+void belowThreehundred(std::map<std::string, int> myMap) {
+    std::cout << "The products under 300: " << std::endl;
+    for (std::map<std::string, int>::iterator it = myMap.begin(); it != myMap.end(); it++) {
+        if (it -> second < 300) {
+            std::cout << it -> first << "; ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+bool for125(std::map<std::string, int> myMap) {
+    for (std::map<std::string, int>::iterator it = myMap.begin(); it != myMap.end(); it++) {
+        if (it -> second == 125) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void cheapestProduct(std::map<std::string, int> myMap) {
+    int temp = 0;
+    std::string tempProduct;
+
+    for (std::map<std::string, int>::iterator it = myMap.begin(); it != myMap.end(); it++) {
+        if (it == myMap.begin()) {
+            temp = it -> second;
+            tempProduct = it -> first;
+        } else if (it -> second < temp) {
+            temp = it -> second;
+            tempProduct = it -> first;
+        }
+    }
+    std::cout << "The cheapest product is: " << tempProduct << "; " << temp << std::endl;
 }
 
 
