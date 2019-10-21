d9d2b33ebf11d295f195e8ac687e370892a84f09
Laci <laszlo.roland.kiss@gmail.com>
2019-10-21 13:54:51 +0200

    finish Guess The Number

--- week-06/day-1/BasicOfC/main.c
+++ week-06/day-1/BasicOfC/main.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    // Write a program that stores 3 sides of a cuboid as variables (doubles)
-    // You should get these variables via console input
-    // The program should write the surface area and volume of the cuboid like:
+
+    // Write a program that stores a number, and the user has to figure it out.
+    // The user can input guesses, after each guess the program would tell one
+    // of the following:
     //
-    // Surface Area: 600
-    // Volume: 1000
+    // The stored number is higher
+    // The stored number is lower
+    // You found the number: 8
 
-    int sideA = 0;
-    int sideB = 0;
-    int sideC = 0;
+    int number = 9;
+    int users_guess = 0;
 
-    int surface = 0;
-    int volume = 0;
-
-    printf("Type the length of the first side!\n");
-    scanf("%d", &sideA);
-    printf("Type the length of the second side!\n");
-    scanf("%d", &sideB);
-    printf("Type the length of the third side!\n");
-    scanf("%d", &sideC);
-
-    surface = 2 * sideA * sideB + 2 * sideB * sideC + 2 * sideC * sideA;
-    volume = sideA * sideB * sideC;
-
-    printf("Surface: %d\n", surface);
-    printf("Volume: %d\n", volume);
+    while (number != users_guess) {
+        printf("Type your guess!\n");
+        scanf("%d", &users_guess);
+        if (users_guess > number) {
+            printf("The stored number is lower.\n");
+        } else if (users_guess < number) {
+            printf("The stored number is higher.\n");
+        }
+    }
+    printf("You found the number: %d", number);
 
     return 0;
 }
