a3f627f50148756a099561969dcfdfff2ca7030f
Laci <laszlo.roland.kiss@gmail.com>
2019-10-21 16:43:38 +0200

    finish Prime

--- week-06/day-1/NoBool/main.c
+++ week-06/day-1/NoBool/main.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int is_even(int numb);
+int is_it_prime(int number);
 
 int main()
 {
-    // Create a program which asks for a number and stores it
-    // Create a function which takes a number as a parameter and
-    // returns 1 if that number is even and returns 0 otherwise
-    // (in this case 0 is an even number)
+    // Create a program which asks for a number and stores it 
+    // Create a function which takes a number as a parameter 
+    // and returns 1 if the number is a prime number and 0 otherwise
+    // (in this case 0 is not considered as a prime number)
 
-    int users_number = 0;
+    int user_number;
 
-    printf("Type your number: ");
-    scanf("%d", &users_number);
+    printf("Type a positive, whole number! ");
+    scanf("%d", &user_number);
 
-    is_even(users_number) ? printf("Your number is even.\n") : printf("Your number is odd.\n");
+    is_it_prime(user_number) ? printf("Your number is prime.\n") : printf("Your number is not prime.\n");
+
     return 0;
 }
 
-int is_even(int numb)
+int is_it_prime(int number)
 {
-    int output = 0;
-    if (numb % 2 == 0)
-        output = 1;
+    int output = 1;
+    int i;
+    for (i = 2; i <= number / 2; i++) {
+        if (number % i == 0) {
+            output = 0;
+            return output;
+        }
+    }
     return output;
 }
