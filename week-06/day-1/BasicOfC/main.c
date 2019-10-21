b6a5f9582e18099337579de23e8cdf7747f895ee
Laci <laszlo.roland.kiss@gmail.com>
2019-10-21 15:13:58 +0200

    finish Separate

--- week-06/day-1/BasicOfC/main.c
+++ week-06/day-1/BasicOfC/main.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PI 3.14
-
-// define a variable called PI with the value of 3.14
-// create a function which takes the radius of a circle as a parameter
-// and return the area of that circle
-// area = radius * radius * PI
-
-float calculate_area(int radius);
+#include "pi.h"
 
 
 int main()
@@ -17,13 +10,8 @@ int main()
     printf("The radius of your circle is: ");
     scanf("%d", &radius);
 
-    printf("The area of your circle is %.2f.", calculate_area(radius));
+    printf("The area of your circle is %.2f.\n", calculate_area(radius));
+    printf("The circumferences of your circle is %.2f.\n", calculate_circumference(radius));
     return 0;
 }
 
-float calculate_area(int radius)
-{
-    float area = 0;
-    area = (float)radius * radius * PI;
-    return area;
-}
