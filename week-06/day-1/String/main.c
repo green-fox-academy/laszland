5c042c385c259b8943edaec4fd9683a9cfed04a1
Laci <laszlo.roland.kiss@gmail.com>
2019-10-21 17:21:31 +0200

    finish Are The Same

--- week-06/day-1/String/main.c
+++ week-06/day-1/String/main.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int count_characters(char * name);
-
+int are_they_identical(char * word1, char * word2);
 
 int main()
 {
-    // Create a program which asks for the name of the user and stores it
-    // Create a function which takes a string as a parameter and returns the length of it
-    // Solve this exercise with and without using string.h functions
+    // Create a program which asks for two strings and stores them
+    // Create a function which takes two strings as parameters and
+    // returns 1 if the two strings are the same and 0 otherwise
+    // Try to erase small and uppercase sensitivity.
 
-    char name_of_user[1024];
+    char user_input1[1024];
+    char user_input2[1024];
 
-    printf("What is your name?\n");
-    scanf("%s", name_of_user);
+    printf("Type me two words!\n");
+    scanf("%s", user_input1);
+    scanf("%s", user_input2);
 
-
-    printf("Wow your name is %d characters long.", count_characters(name_of_user));
+    are_they_identical(user_input1, user_input2) ? printf("They are identical.\n") : printf("They aren't identical.\n");
 
     return 0;
 }
 
-int count_characters(char * name)
+int are_they_identical(char * word1, char * word2)
 {
-    int size = 0;
-    while (*name++) ++size;
+    int output = 1;
+    if (strlen(word1) != strlen(word2)) {
+        output = 0;
+        return output;
+    } else {
+        for (int i = 0; i < strlen(word1); ++i) {
+            if (word1[i] != word2[i]) {
+                output = 0;
+                return output;
+            }
+        }
 
-    /*
-    for (int i = 0; name[i] != '\0'; ++i) {
-        size++;
     }
-     */
-
-    return size;
+    return output;
 }
