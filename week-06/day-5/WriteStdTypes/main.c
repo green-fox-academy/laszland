21767479865a153e467a0f97c63a24aa7b59b669
Laci <laszlo.roland.kiss@gmail.com>
2019-10-25 15:20:30 +0200

    finish Write Std Types

--- week-06/day-5/WriteStdTypes/main.c
+++ week-06/day-5/WriteStdTypes/main.c
@@ -19,16 +19,85 @@ int main() {
      *    they should be the same.
      */
 
+    //- - - WRITE MYINT AS TEXT - - - ///
     char myInt_as_chararray[10];
     sprintf(myInt_as_chararray, "%d", myInt);
 
     FILE* myInt_ptr;
     myInt_ptr = fopen("myInt.txt", "w");
     if(myInt_ptr == NULL)
-        printf("There is an ERROR while opening the file.");
+        return -1;
+
     fprintf(myInt_ptr, "%s", myInt_as_chararray);
 
     fclose(myInt_ptr);
+
+
+    // - - - WRITE MYINT AS BINARY - - - //
+    myInt_ptr = fopen("myInt.bin", "wb");
+    if (myInt_ptr == NULL)
+        return -1;
+
+    fwrite(&myInt, sizeof(int), sizeof(myInt), myInt_ptr);
+
+    fclose(myInt_ptr);
+
+
+    // - - - WRITE MYFLOAT AS BINARY - - - //
+    FILE* myFloat_ptr;
+    myFloat_ptr = fopen("myFloat.bin", "wb");
+    if (myFloat_ptr == NULL)
+        return -1;
+
+    fwrite(&myFloat, sizeof(float), sizeof(myFloat), myFloat_ptr);
+
+    fclose(myFloat_ptr);
+
+
+    // - - - WRITE MYCHAR AS BINARY - - - //
+    FILE* myChar_ptr;
+    myChar_ptr = fopen("myChar.bin", "wb");
+    if (myChar_ptr == NULL)
+        return -1;
+
+    fwrite(&myChar, sizeof(char), sizeof(myChar), myChar_ptr);
+
+    fclose(myChar_ptr);
+
+
+    // - - - READING THE FILES BACK - - - //
+
+    // INT //
+    myInt_ptr = fopen("myInt.bin", "rb");
+    if (myInt_ptr == NULL)
+        return -1;
+
+    fread(&myIntRead, sizeof(int), sizeof(myInt), myInt_ptr);
+
+    fclose(myInt_ptr);
+    printf("%d\n", myIntRead);
+
+
+    // CHAR //
+    myChar_ptr = fopen("myChar.bin", "rb");
+    if(myChar_ptr == NULL)
+        return -1;
+
+    fread(&myCharRead, sizeof(char), sizeof(myChar), myChar_ptr);
+    fclose(myChar_ptr);
+
+    printf("%c\n", myCharRead);
+
+
+    // FLOAT //
+    myFloat_ptr = fopen("myFloat.bin", "rb");
+    if (myFloat_ptr == NULL)
+        return -1;
+
+    fread(&myFloatRead, sizeof(float), sizeof(myFloat), myFloat_ptr);
+    fclose(myFloat_ptr);
+
+    printf("%f\n", myFloatRead);
 
     return 0;
 }
